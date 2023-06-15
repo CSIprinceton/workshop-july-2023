b45340f3613b057266b5624c974a4d80d780cc4e
Pinchen Xie <pinchenx@tigercpu.princeton.edu>
2023-06-15 13:48:54 -0400

    deep wannier

---
+++ hands-on-sessions/day-3/6-deep-wannier/wannier90_example/ice_mlwf_exercise/run.sh
@@ -0,0 +1,23 @@
+ 
+conda activate wannier
+PW=~/QE/q-e-qe-6.4.1/bin/pw.x
+W90=~/wannier90/wannier90-3.1.0/wannier90.x
+PWW90=~/QE/q-e-qe-6.4.1/bin/pw2wannier90.x
+kmesh=~/wannier90/wannier90-3.1.0/utility/kmesh.pl
+
+## run a SCF DFT calculation
+mpirun $PW -input scf.in > scf.out 
+
+## run a non-SCF DFT calculation for getting complete information on orbitals
+## usually we need a denser k-grid for wannierzation. But here we use the sparse 2X2X2 grid to save time. 
+mpirun $PW -input nscf.in > nscf.out 
+ 
+# generate .nnkp as the input of the postprocessing code pw2wannier90
+$W90 -pp water
+
+# produce the matrices needed for maximally localized wannierization .mmn, .amn, .eig…
+mpirun $PWW90 < water.pw2wan > pw2wan.out
+
+#minimize the spread, calculate wannier function
+mpirun $W90 water
+ 
