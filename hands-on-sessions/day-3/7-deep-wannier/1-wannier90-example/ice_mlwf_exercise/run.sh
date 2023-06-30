3c5d93d9b44c861fcf44f1cc27151b02401ab83e
salinelake <salinelakee@gmail.com>
2023-06-29 22:54:55 -0400

    update

--- hands-on-sessions/day-3/7-deep-wannier/1-wannier90-example/ice_mlwf_exercise/run.sh
+++ hands-on-sessions/day-3/7-deep-wannier/1-wannier90-example/ice_mlwf_exercise/run.sh
@@ -1,23 +1,24 @@
- 
-conda activate dp
+conda deactivate # deactivate the 'dp' environment if you haven't done so. Otherwise QE will not work with mpirun. 
 PW=/home/deepmd23admin/Softwares/QuantumEspresso/q-e-qe-6.4.1/bin/pw.x
 W90=/home/deepmd23admin/Softwares/wannier90-3.1.0/wannier90.x
 PWW90=/home/deepmd23admin/Softwares/QuantumEspresso/q-e-qe-6.4.1/bin/pw2wannier90.x
 # kmesh=/home/deepmd23admin/Softwares/wannier90-3.1.0/utility/kmesh.pl
 
 ## run a SCF DFT calculation
-mpirun $PW -input scf.in > scf.out 
+mpirun  -np 4 $PW -input scf.in > scf.out 
 
 ## run a non-SCF DFT calculation for getting complete information on orbitals
 ## usually we need a denser k-grid for wannierzation. But here we use the sparse 2X2X2 grid to save time. 
-mpirun $PW -input nscf.in > nscf.out 
+mpirun  -np 4 $PW -input nscf.in > nscf.out 
  
 # generate .nnkp as the input of the postprocessing code pw2wannier90
 $W90 -pp water
 
 # produce the matrices needed for maximally localized wannierization .mmn, .amn, .eig…
-mpirun $PWW90 < water.pw2wan > pw2wan.out
+mpirun  -np 4 $PWW90 < water.pw2wan > pw2wan.out
 
-#minimize the spread, calculate wannier function
-mpirun $W90 water
+## On this virtual machine, we need to activate this conda environment for wannier90 to work with mpirun. Reasons unknown. 
+conda activate dp
+## minimize the spread, calculate wannier function
+mpirun -np 4 $W90 water
  
--- hands-on-sessions/day-3/7-deep-wannier/3-MD_exercise/run.sh
+++ hands-on-sessions/day-3/7-deep-wannier/3-MD_exercise/run.sh
@@ -1,4 +1,4 @@
-#conda activate deepmd
 ln -s ../train_energy_model/frozen_model.pb frozen_model.pb
 
+conda activate dp
 lmp -v TEMP 330 -v PRES 1.0 -in in.lammps > thermo.log
