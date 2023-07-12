a77f11c07f6af85c4e99f66321a44d00d6e17699
Pinchen Xie <salinelakee@gmail.com>
2023-07-12 03:57:43 -0400

    fix mpirun issue

--- hands-on-sessions/day-3/7-deep-wannier/1-wannier90-example/ice_mlwf_exercise/run.sh
+++ hands-on-sessions/day-3/7-deep-wannier/1-wannier90-example/ice_mlwf_exercise/run.sh
@@ -1,24 +1,24 @@
-conda deactivate # deactivate the 'dp' environment if you haven't done so. Otherwise QE will not work with mpirun. 
+
 PW=/home/deepmd23admin/Softwares/QuantumEspresso/q-e-qe-6.4.1/bin/pw.x
 W90=/home/deepmd23admin/Softwares/wannier90-3.1.0/wannier90.x
 PWW90=/home/deepmd23admin/Softwares/QuantumEspresso/q-e-qe-6.4.1/bin/pw2wannier90.x
 # kmesh=/home/deepmd23admin/Softwares/wannier90-3.1.0/utility/kmesh.pl
 
-## run a SCF DFT calculation
-mpirun  -np 4 $PW -input scf.in > scf.out 
+## run a SCF DFT calculation. DO NOT mpirun directly, call /usr/bin/mpirun instead. The default mpirun on our virtual machine is somehow not doing parallelism at all. 
+/usr/bin/mpirun  -np 6 $PW -input scf.in > scf.out
 
 ## run a non-SCF DFT calculation for getting complete information on orbitals
-## usually we need a denser k-grid for wannierzation. But here we use the sparse 2X2X2 grid to save time. 
-mpirun  -np 4 $PW -input nscf.in > nscf.out 
- 
+## usually we need a denser k-grid for wannierzation. But here we use the sparse 2X2X2 grid to save time.
+/usr/bin/mpirun  -np 6 $PW -input nscf.in > nscf.out
+
 # generate .nnkp as the input of the postprocessing code pw2wannier90
 $W90 -pp water
 
 # produce the matrices needed for maximally localized wannierization .mmn, .amn, .eig…
-mpirun  -np 4 $PWW90 < water.pw2wan > pw2wan.out
+/usr/bin/mpirun  -np 6 $PWW90 < water.pw2wan > pw2wan.out
 
-## On this virtual machine, we need to activate this conda environment for wannier90 to work with mpirun. Reasons unknown. 
+## On this virtual machine, we need to activate this conda environment for wannier90 to work with mpirun.
 conda activate dp
 ## minimize the spread, calculate wannier function
-mpirun -np 4 $W90 water
+/usr/bin/mpirun -np 6 $W90 water
  
